7c49abe9b1f380dfbf9c8dfaa46fc25fcfbebf8a
Jake Vose <jake.vose@gmail.com>
2019-01-19 23:15:38 -0500

    initial checkin

---
+++ RimWorldModListing/MainWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    private int times = 0;
+    private object aws = null;
+    private object zip = null;
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnGo(object sender, EventArgs e)
+    {
+        //loadModFileListings();
+
+        //mapModsConfigToFilePaths();
+
+        //clearOutputDirectory();
+
+        //if (zip != null) { archiveMods(); }
+
+        //if (aws != null) { uploadFiles(); }
+
+        //generateHtmlListing();
+
+        logLine(times++.ToString());
+    }
+
+    private void logLine(String message)
+    {
+        logTextView.Buffer.Insert(logTextView.Buffer.EndIter, message + "\n");
+        logTextView.ScrollToIter(logTextView.Buffer.EndIter, 0, false, 0, 0);
+    }
+}
